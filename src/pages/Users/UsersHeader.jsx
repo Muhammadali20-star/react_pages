2e03d8937d64322f7817ff3cfd88ffd547beb252
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-05-23 07:46:01 +0300

    .

--- src/pages/Users/UsersHeader.jsx
+++ src/pages/Users/UsersHeader.jsx
@@ -6,11 +6,11 @@ const UsersHeader = () => {
     <header className="container mx-auto bg-gray-600 shadow-md text-white py-4 px-6 flex justify-between items-center mb-6">
     <h1 className="text-xl font-bold">MyUsers</h1>
     <nav className="space-x-4">
-      <NavLink to="/" className="text-white hover:text-black lg:hidden">Home</NavLink>
-      <NavLink to="/users" className="text-white hover:text-black underline lg:hidden">Users</NavLink>
-      <NavLink to="/posts" className="text-white hover:text-black lg:hidden">Posts</NavLink>
-      <NavLink to="/recipe" className="text-white hover:text-black lg:hidden">Recipe</NavLink>
-      <NavLink to="/login" className="text-white hover:text-black lg:hidden">Login</NavLink>
+      <NavLink to="/" className="text-white hover:text-black">Home</NavLink>
+      <NavLink to="/users" className="text-white hover:text-black underline">Users</NavLink>
+      <NavLink to="/posts" className="text-white hover:text-black">Posts</NavLink>
+      <NavLink to="/recipe" className="text-white hover:text-black">Recipe</NavLink>
+      <NavLink to="/login" className="text-white hover:text-black">Login</NavLink>
     </nav>
   </header>
   )
